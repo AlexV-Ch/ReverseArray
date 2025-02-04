87be25969d692daa1901f005bab3a94c465ca216
AlexV-Ch <alexch@yandex.ru>
2025-02-04 19:48:17 +0300

    10.6 Практическая работа. Массивы Задание №1 Готово

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,23 @@ import java.util.Collections;
 public class Main {
     public static void main(String[] args) {
 
-        String [] arrayString = new String[] {"a", "b", "c", "d"};
-        System.out.println(Arrays.toString(sortRevers(arrayString)));
+        String[] arrayString = new String[] {"a", "b", "c", "d"};
+    //    System.out.println(Arrays.toString(reverse(arrayString)));
+        System.out.println(Arrays.toString(reverse2(arrayString)));
 
+    //Просто сортировка
     }
-    public static String[] sortRevers ( String[] array) {
+    public static String[] reverse ( String[] array) {
         Arrays.sort(array, Collections.reverseOrder());
         return array;
+    }
+    // Перестановка
+    public static String[] reverse2 (String[] array) {
+        String[] arrayTmp = new String[array.length];
+        int j = array.length;
+        for (int i = 0; i < array.length; i++) {
+            arrayTmp[i] = array[--j];
+        }
+        return arrayTmp;
     }
 }
