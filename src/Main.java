4e0370de10db20033259fd789266c96eb253952c
auto_1 <alexch@yandex.ru>
2025-02-04 16:48:17 +0300

    10.6 Практическая работа Задание №1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        String [] arrayString = new String[] {"a", "b", "c", "d"};
+        System.out.println(Arrays.toString(sortRevers(arrayString)));
+
+    }
+    public static String[] sortRevers ( String[] array) {
+        Arrays.sort(array, Collections.reverseOrder());
+        return array;
     }
 }
